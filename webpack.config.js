6e73eb64d84e0afae1afae2bce66c9b16aacd0af
tonnymuchui <tonykanyingah@gmail.com>
2022-09-30 01:42:24 +0300

    Config images loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,15 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
