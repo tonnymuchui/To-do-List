ae87e865329c860fbe0a5acb64346666a7b1c6a1
RichardChileya <richardchileyawk@gmail.com>
2022-10-06 09:57:02 +0200

    Add test functions for clear and edit task testing

--- src/test.js
+++ src/test.js
@@ -22,12 +22,12 @@ describe('Test if function are working', () => {
     expect(testToDoTask.listArray[0].completed).toBe(true);
   });
 
-  test('Test the edit function is working', () => {
+  test('Test if Edit function is working', () => {
     testToDoTask.editTask(1, 'test1');
     expect(testToDoTask.listArray[0].description).toBe('test1');
   });
 
-  test('Test clearCompleted function is working', () => {
+  test('Test  If clear Completed function is working', () => {
     testToDoTask.clearCompleted();
     expect(testToDoTask.listArray.length).toBe(0);
   });
